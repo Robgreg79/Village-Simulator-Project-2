5974729f4d21988bb81846de40f70c4511e5d6f2
Greg Robinson <robgreg@umich.edu>
2024-05-16 20:37:08 -0400

    first map making

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { Map } from "./Map";
 import { ResourceView } from "./ResourcesView";
-import "./App.css";
+
 
 export function App() {
-  return <></>;
+  return <>
+  <Map/></>;
 }
--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { EditImprovementDialog } from "./EditImprovementDialog";
 import { AddImprovementDialog } from "./AddImprovementDialog";
+import "../Style Sheets/Tile.css";
+
 export function Tile() {
-  return <></>;
+  return (
+    <>
+      <div id="tile"></div>
+    </>
+  );
 }
